6c09a8f310d68ec853abda43ea4152f62b5a32a5
Robert Siurek <robertspabianice23@gmail.com>
2023-05-31 20:59:35 +0200

    Poprawa wizualna oraz README

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# Crypto
+# Cryptocurrencies - Django Project
+
+This is a simple Django web application that provides up-to-date cryptocurrency rates and a currency converter. The application fetches cryptocurrency data from the CoinGecko API, and also includes functionalities for caching and asynchronously fetching data to improve performance.
+
+## Features
+
+1. **Cryptocurrency List:** The application fetches a list of cryptocurrencies along with their current prices in PLN. 
+
+2. **Currency Converter:** Allows conversion of a given amount of a selected cryptocurrency to PLN.
+
+3. **Cryptocurrency Chart:** Displays a chart of a selected cryptocurrency's price over a selected number of days.
+
+## Tech Stack
+
+1. **Back-end:** Django framework.
+
+2. **Front-end:** HTML, CSS, Plotly for data visualization.
+
+3. **API:** CoinGecko API.
+
+4. **Libraries:** PyCoinGecko, Plotly, httpx, Django's built-in caching framework.
+
+## Usage
+
+1. Clone the repository to your local machine.
+
+     ```
+    git clone git@github.com:roberts911/Cryptocurrencies.git
+    ```
+
+2. Install the required packages using pip:
+
+    ```
+    pip install django httpx plotly pycoingecko
+    ```
+
+3. Start the Django server:
+
+    ```
+    python manage.py runserver
+    ```
+
+4. Open a web browser and navigate to `localhost:8000` to access the application.
+
+## Screenshots
+
+## Author
+
+Robert Siurek
--- crypto_app/views.py
+++ crypto_app/views.py
@@ -1,97 +1,89 @@
 from django.core.cache import cache
-import requests
 from django.shortcuts import render
 from pycoingecko import CoinGeckoAPI
-import plotly.graph_objs as go
 import plotly.express as px
 import httpx
 import asyncio
+import requests
+
+BASE_API_URL = "https://api.coingecko.com/api/v3"
+
+def fetch_from_cache_or_api(cache_key, url, cache_time=300):
+    """
+    Fetches data from cache if available, otherwise fetches from API and stores in cache.
+    """
+    data = cache.get(cache_key)
+
+    if data is None:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            cache.set(cache_key, data, cache_time)
+
+    return data
 
 def crypto_list(request):
     """
-    Fetches crypto data from cache if available, otherwise fetches from API and stores in cache.
     Renders the 'crypto_list' template with the fetched crypto data.
     """
-    crypto_data = cache.get('crypto_data')
+    url = f"{BASE_API_URL}/coins/markets?vs_currency=pln&order=market_cap_desc&per_page=30&page=1&sparkline=false"
+    crypto_data = fetch_from_cache_or_api('crypto_data', url)
 
-    if crypto_data is None:
-        url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=pln&order=market_cap_desc&per_page=30&page=1&sparkline=false"
-        response = requests.get(url)
-        crypto_data = response.json()
-        cache.set('crypto_data', crypto_data, 300)  # Cache data for 5 minutes
-
-    context = {'crypto_list': crypto_data}
-    return render(request, 'crypto_app/crypto_list.html', context)
+    return render(request, 'crypto_app/crypto_list.html', {'crypto_list': crypto_data})
 
 def get_crypto_list():
     """
     Returns a list of crypto data, fetching from cache if available, otherwise fetches from API and stores in cache.
-    Each crypto datum is a tuple of (id, symbol, name).
     """
-    crypto_data = cache.get('crypto_data')
+    url = f"{BASE_API_URL}/coins/markets?vs_currency=pln&order=market_cap_desc&per_page=20&page=1&sparkline=false"
+    crypto_data = fetch_from_cache_or_api('crypto_data', url)
 
-    if crypto_data is None:
-        url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=pln&order=market_cap_desc&per_page=20&page=1&sparkline=false"
-        response = requests.get(url)
-        if response.status_code == 200:
-            crypto_data = response.json()
-            cache.set('crypto_data', crypto_data, 300)  # Cache data for 5 minutes
-        else:
-            return []
+    return [(crypto['id'], crypto['symbol'].lower(), crypto['name']) for crypto in crypto_data if crypto] if crypto_data else []
 
-    return [(crypto['id'], crypto['symbol'].lower(), crypto['name']) for crypto in crypto_data]
+def get_conversion_rate(from_currency_id):
+    """
+    Fetches conversion rate for a given cryptocurrency.
+    """
+    url = f"{BASE_API_URL}/simple/price?ids={from_currency_id}&vs_currencies=pln"
+    response = requests.get(url)
+    return response.json().get(from_currency_id, {}).get('pln') if response.status_code == 200 else None
 
 
+    
 def crypto_convert(request):
     """
     Handles the conversion of a given amount of a selected cryptocurrency to another currency.
-    Fetches the list of available cryptocurrencies and conversion rates from an API.
-    On POST request, validates form data and performs conversion.
     Renders the 'crypto_convert' template with conversion results or error messages.
     """
     crypto_list = get_crypto_list()
-
-    if not crypto_list:
-        return render(request, 'crypto_app/crypto_convert.html', {'error_message': 'Nie można pobrać listy kryptowalut. Spróbuj ponownie później.'})
-
+    
     if request.method == 'POST':
         amount = float(request.POST['amount'])
         from_currency_id = request.POST['from_currency']
-        from_currency_symbol = None
+        from_currency_symbol = next((item[1] for item in crypto_list if item[0] == from_currency_id), None)
 
-        for item in crypto_list:
-            if item[0] == from_currency_id:
-                from_currency_symbol = item[1]
-                break
+        if not from_currency_symbol:
+            return render(request, 'crypto_app/crypto_convert.html', {
+                'error_message': 'Unable to find the selected cryptocurrency. Please try again.',
+                'crypto_list': crypto_list
+            })
 
-        if from_currency_symbol is None:
-            return render(request, 'crypto_app/crypto_convert.html', {'error_message': 'Nie można znaleźć wybranej kryptowaluty. Spróbuj ponownie.', 'crypto_list': crypto_list})
-
-        url = f"https://api.coingecko.com/api/v3/simple/price?ids={from_currency_id}&vs_currencies=pln"
-        response = requests.get(url)
-
-        if response.status_code == 200:
-            try:
-                conversion_rate = response.json()[from_currency_id]['pln']
-                converted_amount = amount * conversion_rate
-
-                context = {
-                    'converted_amount': converted_amount,
-                    'from_currency': from_currency_symbol,
-                    'amount': amount,
-                    'crypto_list': crypto_list,
-                }
-                return render(request, 'crypto_app/crypto_convert.html', context)
-            except KeyError:
-                error_message = f"Nie można przeliczyć waluty '{from_currency_symbol}'. Upewnij się, że podałeś prawidłowy symbol kryptowaluty."
+        conversion_rate = get_conversion_rate(from_currency_id)
+        if conversion_rate:
+            return render(request, 'crypto_app/crypto_convert.html', {
+                'converted_amount': amount * conversion_rate,
+                'from_currency': from_currency_symbol,
+                'amount': amount,
+                'crypto_list': crypto_list
+            })
         else:
-            error_message = "Wystąpił błąd podczas łączenia się z API CoinGecko. Spróbuj ponownie później."
-
-        return render(request, 'crypto_app/crypto_convert.html', {'error_message': error_message, 'crypto_list': crypto_list})
+            return render(request, 'crypto_app/crypto_convert.html', {
+                'error_message': "An error occurred while connecting to the CoinGecko API. Please try again later.",
+                'crypto_list': crypto_list
+            })
 
     return render(request, 'crypto_app/crypto_convert.html', {'crypto_list': crypto_list})
 
-cg = CoinGeckoAPI()
 
 async def async_get_coins_list():
     """
@@ -100,12 +92,9 @@ async def async_get_coins_list():
     coins = cache.get('coins_list')
     if not coins:
         async with httpx.AsyncClient() as client:
-            response = await client.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=pln&order=market_cap_desc&per_page=30&page=1&sparkline=false')
-        coins_list = response.json()
-        coins = []
-        for coin in coins_list:
-            coins.append({'id': coin['id'], 'name': coin['name']})
-        cache.set('coins_list', coins, 3600)  # Przechowywanie wyników w cache na 1 godzinę
+            response = await client.get(f'{BASE_API_URL}/coins/markets?vs_currency=pln&order=market_cap_desc&per_page=30&page=1&sparkline=false')
+        coins = [{'id': coin['id'], 'name': coin['name']} for coin in response.json()]
+        cache.set('coins_list', coins, 3600)  # Cache data for 1 hour
     return coins
 
 async def async_get_coin_data(selected_coin, days):
@@ -117,9 +106,9 @@ async def async_get_coin_data(selected_coin, days):
     coin_data = cache.get(cache_key)
     if not coin_data:
         async with httpx.AsyncClient() as client:
-            response = await client.get(f'https://api.coingecko.com/api/v3/coins/{selected_coin}/market_chart?vs_currency=usd&days={days}&interval=daily')
+            response = await client.get(f'{BASE_API_URL}/coins/{selected_coin}/market_chart?vs_currency=pln&days={days}&interval=daily')
         coin_data = response.json()
-        cache.set(cache_key, coin_data, 1800)  # Przechowywanie wyników w cache na 30 minut
+        cache.set(cache_key, coin_data, 1800)  # Cache data for 30 minutes
     return coin_data
 
 def crypto_chart(request):
@@ -128,8 +117,8 @@ def crypto_chart(request):
     Coin data is fetched asynchronously from cache if available, otherwise fetched from API and stored in cache.
     Renders the 'crypto_chart' template with the chart and related data.
     """
-    selected_coin = request.GET.get('coin', 'bitcoin')  # Domyślnie 'bitcoin'
-    selected_days = request.GET.get('days', '30')  # Domyślnie '30'
+    selected_coin = request.GET.get('coin', 'bitcoin')  # Default 'bitcoin'
+    selected_days = request.GET.get('days', '30')  # Default '30'
 
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
@@ -139,17 +128,15 @@ def crypto_chart(request):
 
     loop.close()
 
-    prices = coin_data['prices']
+    prices = coin_data.get('prices', [])
     price_x = [price[0] for price in prices]
     price_y = [price[1] for price in prices]
 
     fig = px.line(x=price_x, y=price_y, labels={'x': 'Date', 'y': 'Price in PLN'}, title=f'{selected_coin.upper()} exchange rate over the past {selected_days} days')
     fig.update_xaxes(type='date')
 
-    plot_div = fig.to_html(full_html=False)
-
     context = {
-        'plot_div': plot_div,
+        'plot_div': fig.to_html(full_html=False),
         'coins': coins,
         'selected_coin': selected_coin,
         'selected_days': selected_days,
